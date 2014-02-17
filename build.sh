ea1f8be60db21e1d79441a8685e94918e343eafe
Graham Christensen <graham@grahamc.com>
2014-02-17 11:10:42 -0600

    Clean up files manually, because vagrant and gitignores don't play nice. Abstract this into a builder repo, that might be better.

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ sudo apt-get -y install gcc-avr binutils-avr avr-libc make git-core
 
 cd /vagrant
 
+for i in `git clean -ndX | grep -v vagrant | cut -d" " -f3`; do
+  rm $i;
+done
+
 cd src
 make
 
